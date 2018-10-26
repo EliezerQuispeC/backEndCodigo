f1c8539d3f9d0874c9408c698ee08c48f9c25648
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-10-26 18:12:22 -0500

    viernes

---
+++ Ejercicios4/Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static int[] LlenarArray()
+        {
+            int[] Arreglo = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            return Arreglo;
+        }
+        
+        static void ImprimirArray(int[]Arreglo)
+        {
+            foreach (int item in Arreglo)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ImprimirArray(LlenarArray());
+            Console.ReadLine();
+        }
+    }
+}
