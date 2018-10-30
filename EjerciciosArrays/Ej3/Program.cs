9571811ad1426a2e5144d75c55fd1fbeb8a19249
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-10-30 17:53:17 -0500

    Ejercicios Array
    Ej4 Recursividad (importante)

---
+++ EjerciciosArrays/Ej3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ej3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimirArray(LlenarArray());
+            Console.ReadKey();
+        }
+        static int[] LlenarArray()
+        {
+            int N = Int16.Parse(Console.ReadLine());
+            int[] Arreglo = new int[N];
+            Random Aleatorio = new Random();
+            for (int i = 0; i < Arreglo.Length; i++)
+            {
+                Arreglo[i] = Aleatorio.Next(0, 20);
+            }
+            return Arreglo;
+        }
+        static void ImprimirArray(int[] Arreglo)
+        {
+            int i = 0, sumatoria = 0;
+            foreach (int item in Arreglo)
+            {
+                Console.WriteLine($"{i.ToString()} => [{item}]");
+                i++;
+                sumatoria = sumatoria + item;
+            }
+            Console.WriteLine($"Sumatoria = {sumatoria}");
+        }
+    }
+}
